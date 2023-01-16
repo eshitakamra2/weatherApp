13718cf3292427d2b87686afd614e3ee0436118e
abc <abc@example.com>
2023-01-16 18:48:01 +0530

    Weather App

---
+++ my-app/src/api.js
@@ -0,0 +1,16 @@
+
+
+
+
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c0838518cdmshc5651ae8677eef9p1e87ddjsn72ace471e878',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f30ee7f991b352906231fd7c1528eb6f";
---
+++ my-app/src/components/curent-weather/current-weather.js
@@ -0,0 +1,44 @@
+import "./current-weather.css"
+const CurrentWeather =({data}) => {
+    return (
+        <div className="weather">
+        <div className ="top"> 
+        <div>
+        <p className="city">{data.city}</p>
+        <p className="weather-decription">{data.weather[0].description}</p>
+        </div>
+        
+        <img alt ="weather" clasName ="weather-icon" src ={`icons/${data.weather[0].icon}.png`}></img>
+        </div>
+        
+            <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="Details">
+        <div className="parameter-row">
+        <span className="parameter-label ">Details ~</span>
+
+        </div>
+<div className="parameter-row">
+<span className="parameter-label">Feels Like</span> 
+<span className="parameter-value">{Math.round(data.main.feels_like)}°C</span> 
+        </div>
+        <div className="parameter-row">
+<span className="parameter-label">Wind</span> 
+<span className="parameter-value">{data.wind.speed} m/s</span> 
+        </div>
+        <div className="parameter-row">
+<span className="parameter-label">Humidity</span> 
+<span className="parameter-value">{data.main.humidity}%</span> 
+        </div>
+        <div className="parameter-row">
+<span className="parameter-label">Pressure</span> 
+<span className="parameter-value">{data.main.pressure} hPa</span> 
+        </div>
+        </div>
+            </div>
+        
+        </div>
+    
+    );
+};
+export default CurrentWeather;
